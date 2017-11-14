8a9058b268b81ba0a298b740c5387592d4736456
tawazlwb <tawazlwb@gmail.com>
2017-11-14 16:02:07 +0100

    The first real app codding from eclipse to GCP - AppEngine

---
+++ MyGCP/src/main/java/com/itis/ikheiry/MyGcpApplication.java
@@ -0,0 +1,12 @@
+package com.itis.ikheiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGcpApplication.class, args);
+	}
+}
